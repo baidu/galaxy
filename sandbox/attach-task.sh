d1babe19dd882ec158fa1257bb2777a008e551eb
bluebore <shiguang31@163.com>
2015-03-31 13:35:55 +0800

    Update attach-task, relica_num=2

--- sandbox/attach-task.sh
+++ sandbox/attach-task.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 rm -rf /tmp/0
-../output/bin/galaxy_client localhost:8102 add task.sh.tar.gz "sh task.sh" 1
+../output/bin/galaxy_client localhost:8102 add task.sh.tar.gz "sh task.sh" 2
