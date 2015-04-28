9ab0f0f88ce9eef3801c8f0a934efa0e6a6b3980
yvxiang <linyvxiang@gmail.com>
2015-04-28 10:25:24 +0800

    Hold lock in WorkspaceManager::GetWorkspace()

--- src/agent/workspace_manager.cc
+++ src/agent/workspace_manager.cc
@@ -118,6 +118,7 @@ int WorkspaceManager::Remove(int64_t task_info_id) {
 }
 
 DefaultWorkspace* WorkspaceManager::GetWorkspace(const TaskInfo& task_info) {
+    MutexLock lock(m_mutex);
     if (m_workspace_map.find(task_info.task_id()) == m_workspace_map.end()) {
         return NULL;
     }
