3320f2b6cc5b472b8ef5ff00d7728129c3799b07
imotai <codego.me@gmail.com>
2015-04-29 11:11:38 +0800

    rename test scripts

--- test/integeration/inte-test-on-local.sh
+++ test/integeration/inte-test-on-local.sh
@@ -6,3 +6,5 @@ export MASTER_BIN_PATH=$GALAXY_SRC_HOME/output/bin/master
 cd $GALAXY_SRC_HOME/console/backend && sh compile-proto.sh && export PYTHONPATH=`pwd`/src
 cd $CUR_DIR && mkdir -p case_tmp
 export GALAXY_CASE_FOLDER=$CUR_DIR/case_tmp
+
+nosetests -vs
