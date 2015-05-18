68bcce2d13188a826f22208c98a4ecf66c7a561b
ontologyzsy <69257877@qq.com>
2015-05-18 23:16:45 +0800

    Revert "Revert "fix bugs""
    This reverts commit 7b5c1e743b827fc705d7d6cbbaa5340d2faee0cf.

--- src/agent/workspace.cc
+++ src/agent/workspace.cc
@@ -23,6 +23,7 @@ DECLARE_string(task_acct);
 namespace galaxy {
 
 int DefaultWorkspace::Create() {
+    const int MKDIR_MODE = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
     LOG(INFO, "create workspace for task %d", m_task_info.task_id());
     if (m_has_created) {
         return 0;
@@ -30,10 +31,10 @@ int DefaultWorkspace::Create() {
     //TODO safe path join
     //create work dir
     std::stringstream private_path;
-    private_path << "/" << m_root_path;
+    private_path << m_root_path;
     int status = 0;
     private_path << "/" << FLAGS_task_acct;
-    status = MakePath(private_path.str().c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    status = MakePath(private_path.str().c_str(), MKDIR_MODE);
     m_task_root_path = private_path.str();
     if (0 != status) {
         LOG(WARNING, "create task root path failed %s err[%d: %s]",
@@ -42,7 +43,7 @@ int DefaultWorkspace::Create() {
     }
 
     private_path << "/" << m_task_info.task_id();
-    status = MakePath(private_path.str().c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    status = MakePath(private_path.str().c_str(), MKDIR_MODE);
 
     if (status == 0) {
         m_task_root_path = private_path.str();
