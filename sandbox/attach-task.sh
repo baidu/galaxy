f77f4a2e479d9429efe94399beed65d7e5a97bfb
imotai <codego.me@gmail.com>
2015-05-25 12:28:58 +0800

    update attach_task.sh

--- sandbox/attach-task.sh
+++ sandbox/attach-task.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 # NOTE ftp path
-../output/bin/galaxy_client add \
-                           --flagfile=client.flag \ 
-                           --job_name=1234 \
-                           --cpu_soft_limit=1 \
-                           --cpu_num=1 --mem_gbytes=1 \
+../output/bin/galaxy_client add --master_addr=localhost:8102 --job_name=1234  \
+                           --cpu_soft_limit=1  \
+                           --cpu_limit=1 --mem_gbytes=1 \
                            --replicate_num=4 \
                            --task_raw=task.sh.tar.gz \
-                           --restrict_tag=dev\
-                           --one_task_per_host\
+                           --one_task_per_host \
                            --cmd_line="sh task.sh"
