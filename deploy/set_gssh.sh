32a387cdde222160b795975d0e52ede51b5c12bd
fxsjy <ccnusjy@gmail.com>
2015-05-25 18:33:43 +0800

    ci pam_galaxy.cc

--- deploy/set_gssh.sh
+++ deploy/set_gssh.sh
@@ -5,8 +5,13 @@ if [ $# -ne 2 ]; then
 fi
 so_path=$1
 password_dir=$2
+cp $so_path /lib64/security/libpam_galaxy.so
+if [ $? -ne 0 ]; then
+	echo "faild to cp the so"
+	exit 1
+fi
 sed -ine '/galaxy.so/d' /etc/pam.d/sshd
-sed -ine "2i\auth     sufficient   $so_path $password_dir" /etc/pam.d/sshd
+sed -ine "2i\auth     sufficient   libpam_galaxy.so $password_dir" /etc/pam.d/sshd
 if [ $? -ne 0 ]; then
     echo "faild to attach libpam_galaxy.so"
 fi
