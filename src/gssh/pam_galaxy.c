c66d14cb4d4cf9413205076c96be8b7ec3cde99a
fxsjy <ccnusjy@gmail.com>
2015-05-27 14:07:51 +0800

    only allow DoorDog group

--- src/gssh/pam_galaxy.c
+++ src/gssh/pam_galaxy.c
@@ -8,8 +8,9 @@
 //
 // auth     sufficient     pam_galaxy.so [directory to store users' temporary passwords]
 // e.g.
-//   auth     sufficient     /root/galaxy_tools/pam_galaxy.so /root/gssh
-//   edit /root/gssh/sunjunyi01.pwd, write 1234.
+//   auth     sufficient     /root/galaxy_tools/pam_galaxy.so
+//
+//   edit /tmp/sunjunyi01.pwd, write 1234.
 //   then, sunjunyi01 can login with password 1234.(only once)
 //
 #include <syslog.h>
@@ -17,10 +18,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <security/pam_modules.h>
 #include <security/_pam_types.h>
+#include <pwd.h>
 
 #define SSH_PWD_PATH "/tmp/"
+#define DOOR_DOG_GID 100000
 
 int GetUser(pam_handle_t *pamh, const char **user); 
 int GetPasswd(pam_handle_t *pamh, char **passwd); 
@@ -31,7 +35,8 @@ extern "C" {
 
 PAM_EXTERN int
 pam_sm_authenticate(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[]) {
+    int argc, const char *argv[])
+{
     (void)flags;
     openlog("galaxy", LOG_CONS | LOG_PID, LOG_USER);
     const char* user;
@@ -40,6 +45,12 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
     char pwd_set[1024];
     FILE* fh;
     const char* pwd_path = NULL;
+    struct passwd user_info;
+    struct passwd* user_result;
+    char* user_info_buf;
+    int user_buf_size = sysconf(_SC_GETPW_R_SIZE_MAX);
+    int user_info_ret = 0;
+    long user_group_id = 0;
     if (argc > 0) {
         pwd_path = argv[0];
     } else {
@@ -53,8 +64,19 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
         closelog();
         return PAM_AUTHINFO_UNAVAIL;
     }
-    syslog(LOG_INFO, "user try login to galaxy: %s", user);
+    user_info_buf = malloc(user_buf_size);
+    user_info_ret = getpwnam_r(user, &user_info, user_info_buf, user_buf_size, &user_result);
+    if (user_info_ret == 0 && user_result != NULL) {
+        syslog(LOG_INFO, "group id %d", user_info.pw_gid);
+        user_group_id = (long)user_info.pw_gid;
+    }
+    free(user_info_buf);
 
+    if (user_group_id != DOOR_DOG_GID) {
+        syslog(LOG_INFO, "%s login failed, because its group is not doordog", user);
+        return PAM_AUTHINFO_UNAVAIL;
+    }
+    syslog(LOG_INFO, "user try login to galaxy: %s", user);
     //fetch password
     ret = GetPasswd(pamh, &passwd);
     if (ret != PAM_SUCCESS) {
@@ -122,6 +144,7 @@ pam_sm_open_session(pam_handle_t *pamh, int flags,
     (void)argc;
     (void)argv;
     return (PAM_SUCCESS);
+
 }
 
 PAM_EXTERN int
@@ -133,6 +156,7 @@ pam_sm_close_session(pam_handle_t *pamh, int flags,
     (void)argc;
     (void)argv;
     return (PAM_SUCCESS);
+
 }
 
 PAM_EXTERN int
@@ -144,6 +168,7 @@ pam_sm_chauthtok(pam_handle_t *pamh, int flags,
     (void)argc;
     (void)argv;
     return (PAM_SUCCESS);
+
 }
 
 #ifdef __cplusplus
@@ -204,3 +229,5 @@ int GetPasswd(pam_handle_t *pamh, char **passwd) {
         return PAM_AUTH_ERR;
     return pam_err;
 }
+
+
