b6fd6031c626a825d83d26dd33c6420677317690
fxsjy <ccnusjy@gmail.com>
2015-06-15 11:27:57 +0800

    fix merge-conflict

--- src/flags.cc
+++ src/flags.cc
@@ -42,7 +42,6 @@ DEFINE_int32(agent_cgroup_clear_retry_times, 20, "how many times for retry destr
 DEFINE_int32(agent_app_stop_wait_retry_times, 10, "how many times for stop wait");
 DEFINE_string(monitor_conf_path, "", "path of monitor conf");
 
-DEFINE_int32(agent_cgroup_clear_retry_times, 50, "how many times for retry destroy cgroup");
 DEFINE_string(pam_pwd_dir, "/tmp/", "directory that stores galaxy-ssh passwords on agent node");
 
 /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
