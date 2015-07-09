f8b33adcb33b33291d82407313254e20d4542cba
imotai <codego.me@gmail.com>
2015-07-09 17:30:53 +0800

    fix fe

--- console/ui/app/scripts/controllers/main.js
+++ console/ui/app/scripts/controllers/main.js
@@ -275,6 +275,21 @@ angular.module('galaxy.ui.ctrl').controller('NewJobModalCtrl',function($scope,
     $scope.triggerList = [];
     $scope.actionList = [];
     $scope.ruleList = [];
+    $scope.defaultPkgType = [{name:'FTP',id:0},{name:'HTTP',id:1},{name:'P2P',id:2},{name:'BINARY',id:3}];
+    $scope.deployTpl = {
+        groupId:"",
+        name:"",
+        startCmd:"",
+        tag:"",
+        pkgType:0,
+        pkgSrc:"",
+        deployStepSize:10,
+        replicate:0,
+        memoryLimit:3,
+        cpuShare:0.5,
+        oneTaskPerHost:false,
+        logInput:null
+    };
 
     $scope.disableSubmit=false;
     $scope.alerts = [];
@@ -382,7 +397,72 @@ angular.module('galaxy.ui.ctrl').controller('NewJobModalCtrl',function($scope,
             action:$scope.ruleTpl.action
         });
         $scope.resetEditor();
+    };
+    $scope.selectGroup = null;
+    $scope.groupUpdate = function(){
+      if ($scope.selectGroup != null){
+          $http.get(config.rootPrefixPath + "quota/groupstat?id="+ $scope.selectGroup.id)
+               .success(function(data){
+                $scope.groupStat = data.data.stat;
+                $scope.deployTpl.groupId = $scope.selectGroup.id;
+            });
+      }
     }
+    $scope.ok = function () {
+        $scope.alerts = [];
+        $scope.deployTpl.rules = $scope.ruleTpl;
+        $cookies.lastServiceForm = JSON.stringify($scope.deployTpl);
+        if ($scope.groupStat == null ){
+            $scope.alerts.push({msg: '请选择quota组'});
+            return ;
+         }
+        var totalCpuRequire = $scope.deployTpl.replicate * $scope.deployTpl.cpuShare;
+        var totalMemRequire = $scope.deployTpl.replicate * $scope.deployTpl.memoryLimit * 1024 * 1024 * 1024;
+
+        if(totalCpuRequire > $scope.groupStat.total_cpu_left){
+            $scope.alerts.push({msg: 'cpu超出总配额'});
+            return;
+        }
+        if(totalMemRequire > $scope.groupStat.total_mem_left){ 
+            $scope.alerts.push({msg: '内存超出总配额'});
+            return;
+        }
+        $scope.disableBtn=true;
+        $http({
+            method:"POST",
+            url:config.rootPrefixPath + 'service/create?master='+config.masterAddr, 
+            data:$scope.deployTpl,
+            headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj){
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                }
+                return str.join("&");
+            }
+        }).success(function(data, status, headers, config) {
+            $scope.disableBtn=false;
+            if(data.status==0){
+                notify({ message:'创建服务成功'} );
+                $route.reload();
+                $modalInstance.close();
+            }else{
+                $scope.alerts.push({ type: 'danger', msg: '错误：'+data.msg });
+            }
+        }).error(function(data, status, headers, config) {
+            $scope.disableBtn=false;
+
+        });
+    };
+
+     $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+     };
+     $scope.closeAlert = function(index) {
+         $scope.alerts.splice(index, 1);
+     };
+
+
 
 });
 angular.module('galaxy.ui.ctrl').controller('CreateServiceModalInstanceCtrl',
