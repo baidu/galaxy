29aca22dd07af2747971a4e76e7f7b74ac0acaed
bluebore <shiguang31@163.com>
2015-07-31 18:24:09 +0800

    Add rpc_client

---
+++ src/flags.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2014, Baidu.com, Inc. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gflags/gflags.h>
+
+// master
+DEFINE_string(master_port, "7828", "Master service listen port");
+
+// scheduler
+
+// agent
+
+// gce
