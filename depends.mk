38889155db97a34ed8c2175f49924ce266385683
bluebore <shiguang31@163.com>
2015-08-01 11:44:27 +0800

    Update depends.mk

--- depends.mk
+++ depends.mk
@@ -1,9 +1,9 @@
 
-SNAPPY_PATH=../../../third-64/snappy/
-PROTOBUF_PATH=../../../third-64/protobuf/
-PROTOC_PATH=../../../third-64/protobuf/bin/
+SNAPPY_PATH=
+PROTOBUF_PATH=
+PROTOC_PATH=
 PROTOC=$(PROTOC_PATH)protoc
-PBRPC_PATH=../../../public/sofa-pbrpc/output/
-BOOST_PATH=../../../third-64/boost/
-GFLAGS_PATH=../../../third-64/gflags/
+PBRPC_PATH=./thirdparty/sofa-pbrpc/output/
+BOOST_PATH=
+GFLAGS_PATH=
 
