c3b7479b6986bcff1294dd00f176f4c8f221a4fa
cyshi <wbstsa@gmail.com>
2015-08-02 09:31:23 +0800

    add pod manager

---
+++ src/agent/pod_manager.cc
@@ -0,0 +1,31 @@
+#include "pod_manager.h"
+
+namespace baidu {
+namespace galaxy {
+
+int PodManager::Run(const PodDesc& pod) {
+
+    std::map<std::string, InitdHandler*>::iterator it;
+    {
+        MutexLock lock(&handlers_mutex_);
+        it = pod_handlers_.find(pod.podid);
+        if (it == pod_handlers)
+    }
+
+}
+
+int PodManager::Kill(const PodDesc& pod) {
+
+}
+
+int PodManager::Query(const std::string& podid, 
+                      PodInfo* info)  {
+
+}
+
+int PodManager::List(std::vector<std::string>* pod_ids) {
+
+}
+
+}
+}
