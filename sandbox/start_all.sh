9fb9ec423347eab9f18f663b294e18a62a6adf21
cyshi <wbstsa@gmail.com>
2015-08-02 12:11:51 +0800

    update start up script

--- sandbox/start_all.sh
+++ sandbox/start_all.sh
@@ -3,6 +3,7 @@ cd ../ins/sandbox && nohup ./start_all.sh &> ins_start.log &
 sleep 2
 echo "start master"
 nohup  ../master --flagfile=galaxy.flag >master.log 2>&1 &
+sleep 2
 echo "start agent"
 nohup  ../agent --flagfile=galaxy.flag >agent.log 2>&1 &
 
