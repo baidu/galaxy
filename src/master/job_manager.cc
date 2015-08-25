e7efe2b81f4bcf3a06b893914385548d01159928
imotai <codego.me@gmail.com>
2015-08-25 17:11:21 +0800

    do not change stage in safe mode

--- src/master/job_manager.cc
+++ src/master/job_manager.cc
@@ -608,7 +608,8 @@ void JobManager::QueryAgentCallback(AgentAddr endpoint, const QueryRequest* requ
             PodStatus* pod = new PodStatus();
             pod->CopyFrom(report_pod_info);
             pod->set_stage(state_to_stage_[pod->state()]);
-            jobs_[jobid]->pods_[podid] = pod;
+            pod->set_endpoint(report_agent_info.endpoint());
+            jobs_[jobid]->pods_[podid] = pod; 
             pods_not_on_agent[jobid][podid] = pod;
             pods_on_agent_[endpoint][jobid][podid] = pod;
         }
@@ -844,6 +845,10 @@ void JobManager::ChangeStage(const PodStage& to,
                              PodStatus* pod,
                              Job* job) {
     mutex_.AssertHeld();
+    if (safe_mode_) { 
+        LOG(WARNING, "master does not support stage change in safe mode");
+        return;
+    }
     if (pod == NULL || job == NULL) {
         LOG(WARNING, "change pod invalidate input");
         return;
