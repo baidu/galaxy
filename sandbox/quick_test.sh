8391acefcefceb1a6f14047838c5cbbb466ede63
imotai <codego.me@gmail.com>
2015-12-02 15:53:07 +0800

    sdk update

--- sandbox/quick_test.sh
+++ sandbox/quick_test.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 set -e
 
-./clear.sh
+./clear.sh >/dev/null 2>&1 
 [ -d gc_dir ] || mkdir gc_dir 
-
 hn=`hostname`
 echo "--master_port=7810" > galaxy.flag
 echo "--master_host=127.0.0.1" >> galaxy.flag
 echo "--agent_port=7182" >> galaxy.flag
-echo "--agent_cpu_share=1000000" >> galaxy.flag
-echo "--agent_mem_share=1000000" >> galaxy.flag
+echo "--agent_millicores_share=15000" >> galaxy.flag
+echo "--agent_mem_share=68719476736" >> galaxy.flag
 
 echo "--nexus_servers=$hn:8868,$hn:8869,$hn:8870,$hn:8871,$hn:8872" >> galaxy.flag
 echo "--agent_initd_bin=`pwd`/../initd" >> galaxy.flag
