5777d426fecc739da0992cb2d48d796ff2b8c895
haolifei <haolifei@baidu.com>
2016-05-28 11:51:03 +0800

    add file_stream.cc

---
+++ src/agent/util/file_stream.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+#include "error_code.h"
+#include "boost/noncopyable.hpp"
+#include <stdio.h>
+
+namespace baidu {
+namespace galaxy {
+namespace file {
+
+class FileInputStream : public boost::noncopyable {
+public:
+    explicit FileInputStream(const std::string& path);
+    ~FileInputStream();
+
+    bool IsOpen();
+    bool Eof();
+    void GetLastError(baidu::galaxy::util::ErrorCode& ec);
+    baidu::galaxy::util::ErrorCode ReadLine(std::string& line);
+    baidu::galaxy::util::ErrorCode Read(void* buf, size_t& size);
+
+private:
+    std::string path_;
+    FILE* file_;
+    bool opened_;
+    int errno_; // last_erro
+};
+}
+}
+}
