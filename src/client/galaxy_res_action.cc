619ecfba3d4b134427317c16d325d09f492b41ff
zhoumeiting <zmtmei@gmail.com>
2016-05-31 16:02:23 +0800

    add options for show_agent

--- src/client/galaxy_res_action.cc
+++ src/client/galaxy_res_action.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
 #include <tprinter.h>
 #include "galaxy_res_action.h"
 
